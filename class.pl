9ce8f39540deeeb1c250bc9d582013f98c60729d
frutastontas <joaopedrofurtado2005@gmail.com>
2026-01-02 16:41:18 +0000

    NNN

--- class.pl
+++ class.pl
@@ -8,21 +8,53 @@ class(ipc, tp, '4 Thu', 16, 1.5).
 class(fsi, t, '1 Mon', 10.5, 2). 
 class(fsi, tp, '5 Fri', 8.5, 2). 
 class(rc, t, '5 Fri', 10.5, 2). 
-class(rc, tp, '1 Mon', 8.5, 2). 
+class(rc, tp, '1 Mon', 8.5, 2).
+
 
 same_day(Course1, Course2) :-
-    class(Course1, _, Day, _, _),  % Encontra o dia da primeira cadeira
-    class(Course2, _, Day, _, _).  % Verifica se a segunda cadeira tem aula nesse MESMO dia
-
+    class(Course1,_,Day,_,_),
+    class(Course2,_,Day,_,_).
 
 daily_courses(Day, Courses) :-
-            findall(Course,class(Course,_,Day,_,_), Courses).
+    findall(Course, class(Course,_,Day,_,_), Courses).
 
 
 short_classes(L) :-
-            findall(Course-Day/Time, (class(Course,_,Day,Time,Duration), Duration < 2), L).
+    setof(Course-DayOfWeek/Time,ClassType^Duration^(class(Course,ClassType,DayOfWeek,Time,Duration),Duration < 2),L).    
+
+course_classes(Course, Classes) :-
+    findall(DayOfWeek/Time-ClassType,class(Course,ClassType,DayOfWeek,Time,_),Classes).
+
+courses(L) :-
+    setof(Course,ClassType^DayOfWeek^Time^Duration^(class(Course,ClassType,DayOfWeek,Time,Duration)),L).
 
 
 schedule :-
-        setof((DayOfWeek,Time,Course,ClassType,Duration),class(Course,ClassType,DayOfWeek,Time,Duration), List).
+    setof(DayOfWeek-Course-ClassType,Time^Duration^(class(Course,ClassType,DayOfWeek,Time,Duration)),List),
+    display_class(List).
 
+display_class([]).
+display_class([DayOfWeek-Course-ClassType|Rest]) :-
+    format('Course-~w, Type-~w, Day-~w ~n',[Course,ClassType,DayOfWeek]),
+    display_class(Rest).
+
+
+find_class :-
+    write('Give us a day: '),
+    read(_X),nl,
+    write('Give us a time: '),
+    read(_Y),nl,
+    setof(Course-Time-Duration,ClassType^(class(Course,ClassType,_X,Time,Duration)),List),
+    taking_place(List,_Y,Result),
+    write(Result).
+
+taking_place([],_,[]).
+
+taking_place([Course-Time-Duration|Rest],GivenTime,[Course-Time-Duration|Result]) :-
+    Time =< GivenTime,
+    EndTime is Time + Duration,
+    EndTime > GivenTime, !,
+    taking_place(Rest,GivenTime,Result).
+
+taking_place([_|Rest],GivenTime,Result) :-
+    taking_place(Rest,GivenTime,Result).
